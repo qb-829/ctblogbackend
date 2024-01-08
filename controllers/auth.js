4ca556a7cbfae4426b0d103f17dc2c57a3d839f2
qb-829 <publiqproperty@gmail.com>
2024-01-07 19:14:11 -0500

    update to auth for login

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,7 +31,21 @@ export const register = (req,res)=>{
 }
 
 export const login = (req,res)=>{
-    
+    //CHECK FOR EXISTING USER
+
+    const q = "SELECT * FROM users WHERE username = ?"
+
+    db.query(q,[req.body.username], (err,data)=>{
+        if (err) return res.json(err)
+        if(data.length === 0) return res.status(404).json("User not found")
+
+        //CHECK PASSWORD
+
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password)
+
+        if(!isPasswordCorrect) return res.status(400).json("Wrong username or password")
+    })
+     
 }
 
 export const logout = (req,res)=>{
