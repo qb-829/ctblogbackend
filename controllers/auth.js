3d9a6e01754a3acef31bf8657d9542a9214991d2
qb-829 <publiqproperty@gmail.com>
2024-01-13 00:06:35 -0500

    adding and accessing user posts

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,5 +59,10 @@ export const login = (req,res)=>{
 }
 
 export const logout = (req,res)=>{
+
+    res.clearCookie("access_token",{
+        sameSite:"none",
+        secure:true
+    }).status(200).json("User logged out.")
     
 }
